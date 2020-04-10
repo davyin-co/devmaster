70f0f03a196f1ae3828761ef84cbb9b7d9bd79cb
ipumpkin <ipumpkin@foxmail.com>
2020-04-10 10:40:15 +0800

    Update hosting_site_backup_manager.js

--- modules/aegir/hosting_site_backup_manager/hosting_site_backup_manager.js
+++ modules/aegir/hosting_site_backup_manager/hosting_site_backup_manager.js
@@ -1,19 +1,22 @@
-hostingSiteBackupManagerRefreshList = function() {
-  if (!Drupal.settings.hostingSiteBackupManager.nid) {
-    return null;
-  }
+(function ($) {
+  Drupal.behaviors.devel = {
+    attach: function (context, settings) {
+      hostingSiteBackupManagerRefreshList = function() {
+      if (!settings.hostingSiteBackupManager.nid) {
+        return null;
+      }
 
-  var hostingSiteBackupManagerCallback = function(data, responseText) {
-      $("#hosting-site-backup-manager-backupstable").html(data.markup);
-      setTimeout("hostingSiteBackupManagerRefreshList()", 30000);
-  }
+      var hostingSiteBackupManagerCallback = function(data, responseText) {
+        $("#hosting-site-backup-manager-backupstable").html(data.markup);
+          setTimeout("hostingSiteBackupManagerRefreshList()", 30000);
+        }
 
-  hostingTaskAddOverlay('#hosting-site-backup-manager-backupstable');
-  $.get(Drupal.settings.basePath + 'node/' + Drupal.settings.hostingSiteBackupManager.nid + '/ajax/backups', null, hostingSiteBackupManagerCallback , 'json');
-}
-
-$(document).ready(function() {
-  if("#hosting-site-backup-manager-backupstable") {
-    setTimeout("hostingSiteBackupManagerRefreshList()", 30000);
+        //hostingTaskAddOverlay('#hosting-site-backup-manager-backupstable');
+        $.get(Drupal.settings.basePath + 'node/' + settings.hostingSiteBackupManager.nid + '/ajax/backups', null, hostingSiteBackupManagerCallback , 'json');
+      }
+      if($("#hosting-site-backup-manager-backupstable")) {
+        setTimeout("hostingSiteBackupManagerRefreshList()", 30000);
+      }
+    }
   }
-});
+})(jQuery);
